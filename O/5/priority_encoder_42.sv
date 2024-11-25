bfcaaeffb18c3f0d8c21bb4823c1a749c3c0003d
Ananya Verma <165065804+PES2UG23EC017@users.noreply.github.com>
2024-11-25 17:19:27 +0530

    Update priority_encoder_42.sv

--- O/5/priority_encoder_42.sv
+++ O/5/priority_encoder_42.sv
@@ -1,12 +1,6 @@
 module pri_en42(a,y);
 input logic[3:0] a;
 output logic[1:0] y;
-always_comb
-    casez(a)
-        4'b1???: y=2'b11;
-        4'b01??: y=2'b10;
-        4'b001?: y=2'b01;
-        4'b0001: y=2'b00;
-        default: y=2'bxx;
-    endcase
+assign y[1]=a[2]|a[3];
+assign y[0]=a[3]|((~a[2])&a[1]);
 endmodule
