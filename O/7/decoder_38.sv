99490354b9f3176ea233f468e414b7ad87629b69
Ananya Verma <165065804+PES2UG23EC017@users.noreply.github.com>
2024-11-25 17:53:12 +0530

    Update decoder_38.sv

--- O/7/decoder_38.sv
+++ O/7/decoder_38.sv
@@ -1,15 +1,12 @@
 module decoder(a,y);
 input logic[2:0] a;
 output logic[7:0] y;
-always_comb
-    case(a)
-        3'b000: y=8'b0000_0001;
-        3'b001: y=8'b0000_0010;
-        3'b010: y=8'b0000_0100;
-        3'b011: y=8'b0000_1000;
-        3'b100: y=8'b0001_0000;
-        3'b101: y=8'b0010_0000;
-        3'b110: y=8'b0100_0000;
-        3'b111: y=8'b1000_0000;
-    endcase
+assign y[0]=(~a[2])&(~a[1])&(~a[0]);
+assign y[1]=(~a[2])&(~a[1])&a[0];
+assign y[2]=(~a[2])&a[1]&(~a[0]);
+assign y[3]=(~a[2])&a[1]&a[0];
+assign y[4]=a[2]&(~a[1])&(~a[0]);
+assign y[5]=a[2]&(~a[1])&a[0];
+assign y[6]=a[2]&a[1]&(~a[0]);
+assign y[7]=a[2]&a[1]&a[0];
 endmodule
